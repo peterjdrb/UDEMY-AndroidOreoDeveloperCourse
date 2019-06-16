44a4acfc0e03e7d5e042cc34eb5a11f5a08107bf
Peter Robertson <peterjdrb@outlook.com>
2019-06-16 15:44:05 +0100

    If statements

---
+++ Section03/31.IfStatements/ifStatements.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		int[] numbers = {23,45};
+		
+		if (numbers[1] > numbers[0]) {
+			System.out.println("Second nubmer is greater than the first");
+		} else {
+			System.out.println("The first nubmer is less than or equal to the second.");
+		}
+	}
+}
