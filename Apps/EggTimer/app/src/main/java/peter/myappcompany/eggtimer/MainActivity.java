a6e7deb914978952d861d318fb61e03edd2da11d
Peter Robertson <peterjdrb@outlook.com>
2019-09-13 11:48:08 +0100

    Egg Timer
    Section 05 - Lecture 068

---
+++ Apps/EggTimer/app/src/main/java/peter/myappcompany/eggtimer/MainActivity.java
@@ -0,0 +1,117 @@
+package peter.myappcompany.eggtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static java.lang.Math.floor;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar timerBar;
+    TextView timerText;
+    MediaPlayer sound;
+    CountDownTimer timer;
+
+    final int defaultTime = 30;
+    final int secondsPerMin = 60;
+    final int maxMinutes = 10;
+    final int minSeconds = 0;
+    final int maxSeconds = secondsPerMin * maxMinutes;
+
+    int currentTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initialSetup();
+    }
+
+    private void initialSetup() {
+        //SetSeekBar with its min, max and progress.
+        timerBar = findViewById(R.id.timerBar);
+        timerBar.setMin(minSeconds);
+        timerBar.setMax(maxSeconds);
+        timerBar.setProgress(defaultTime);
+        currentTime = timerBar.getProgress();
+
+        timerBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                currentTime = i;
+                setTimerText();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        //Set timerText;
+        timerText = findViewById(R.id.txtTimer);
+        setTimerText();
+    }
+
+    private void setTimerText() {
+        int minutes = (currentTime / secondsPerMin);
+        int seconds = currentTime - (secondsPerMin*minutes);
+
+        String strSeconds = Integer.toString(seconds);
+        if (strSeconds.length() == 1) {
+            strSeconds = "0" + seconds;
+        }
+
+        timerText.setText(Integer.toString(minutes) + ":" + strSeconds);
+    }
+
+    public void btnGoPressed (View view){
+        Button btn = findViewById(R.id.btnGo);
+        int tag = Integer.parseInt(btn.getTag().toString());
+
+        if (tag == 1) {
+            timerBar.setEnabled(false);
+            btn.setTag(2);
+            btn.setText("Stop");
+
+            timer = new CountDownTimer(currentTime*1000,1000){
+
+                @Override
+                public void onTick(long millisecondsUntilDone) {
+                    currentTime = currentTime - 1;
+                    setTimerText();
+                }
+
+                @Override
+                public void onFinish() {
+                    sound = MediaPlayer.create(MainActivity.this, R.raw.alarm);
+                    sound.start();
+                }
+            }.start();
+
+        } else {
+            timerBar.setEnabled(true);
+            btn.setTag(1);
+            btn.setText("Go!");
+            timer.cancel();
+
+        }
+    }
+}
