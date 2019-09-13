7de45d9cfb81f4ef28b10707aaf119044fdb87f8
Peter Robertson <peterjdrb@outlook.com>
2019-09-13 14:58:15 +0100

    Showing and Hiding Elements
    Section 05 - Lecture 070

---
+++ Section05/070.Showing&HidingElements/app/src/main/java/peter/myappcompany/hidingelements/MainActivity.java
@@ -0,0 +1,29 @@
+package peter.myappcompany.hidingelements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void showText (View view){
+        textView.setVisibility(View.VISIBLE);
+    }
+
+
+    public void hideText (View view){
+        textView.setVisibility(View.INVISIBLE);
+    }
+
+}
